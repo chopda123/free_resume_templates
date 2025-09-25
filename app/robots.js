4bfe7825a415486bd7893fa89574ab7e73cc5f56
yash bansod <ybbansod@mitaoe.ac.in>
2025-09-25 15:10:00 +0530

    sitemsap updates-2

--- app/robots.js
+++ app/robots.js
@@ -1,11 +1,17 @@
-export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
-    sitemap: "https://freeresume.shop/sitemap.xml",
-  };
+// app/robots.js
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const content = `
+User-agent: *
+Allow: /
+
+Sitemap: https://freeresume.shop/sitemap.xml
+`;
+
+  return new NextResponse(content, {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
 }
